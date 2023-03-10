aad176309f4cc19b9f0d676b9fe91e2a8de035db
VirtueSky <virtuesky.0520@gmail.com>
2023-03-10 22:45:15 +0700

    *remove ui effect in popup and add uiGradien

--- UI/Popup.cs
+++ UI/Popup.cs
@@ -3,66 +3,43 @@ using UnityEngine;
 
 namespace Virtuesky.common
 {
-    public class Popup : BasePopup
+    public class Popup : MonoBehaviour
     {
-        public override void Show()
+        public CanvasGroup canvasGroup => GetComponent<CanvasGroup>();
+        public Canvas Canvas => GetComponent<Canvas>();
+
+        public virtual void Show()
         {
             BeforeShow();
             gameObject.SetActive(true);
-            if (UseShowAnimation)
-            {
-                switch (ShowAnimationType)
-                {
-                    case ShowAnimationType.OutBack:
-                        DOTween.Sequence().OnStart(() => Container.transform.localScale = Vector3.one * .9f)
-                            .Append(Container.transform.DOScale(Vector3.one, durationPopup)
-                                .SetEase(Ease.OutBack).OnComplete(() => { AfterShown(); }));
-                        break;
-                    case ShowAnimationType.Flip:
-                        DOTween.Sequence().OnStart(() => Container.transform.localEulerAngles = new Vector3(0, 180, 0))
-                            .Append(Container.transform.DORotate(Vector3.zero, durationPopup))
-                            .SetEase(Ease.Linear).OnComplete(() => { AfterShown(); });
-                        break;
-                }
-            }
-            else
-            {
-                AfterShown();
-            }
+            AfterShown();
         }
 
-        public override void Hide()
+        public virtual void Hide()
         {
             BeforeHide();
-            if (UseHideAnimation)
-            {
-                switch (HideAnimationType)
-                {
-                    case HideAnimationType.InBack:
-                        DOTween.Sequence().Append(Container.transform
-                            .DOScale(Vector3.one * .7f, durationPopup).SetEase(Ease.InBack)
-                            .OnComplete(() =>
-                            {
-                                gameObject.SetActive(false);
-                                AfterShown();
-                            }));
-                        break;
-                    case HideAnimationType.Fade:
-                        // canvasGroup.DOFade(0, durationPopup).OnComplete(() =>
-                        // {
-                        //     canvasGroup.alpha = 1;
-                        //     gameObject.SetActive(false);
-                        //     AfterHidden();
-                        // });
-                        AfterHidden();
-                        break;
-                }
-            }
-            else
-            {
-                gameObject.SetActive(false);
-                AfterHidden();
-            }
+            gameObject.SetActive(false);
+            AfterHidden();
+        }
+
+        public virtual void AfterInstantiate()
+        {
+        }
+
+        public virtual void BeforeShow()
+        {
+        }
+
+        public virtual void AfterShown()
+        {
+        }
+
+        public virtual void BeforeHide()
+        {
+        }
+
+        public virtual void AfterHidden()
+        {
         }
     }
 }
